b48fc743a6f4e8353da1e97ac14d93ffce596eeb
zhsyourai <zhsyourai@163.com>
2017-11-06 23:54:30 +0800

    Fix bugs

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,14 +8,14 @@ export const AppRoutes: Routes = [{
   component: MainLayoutComponent,
   children: [{
     path: '',
-    loadChildren: './pages/content/content.module#ContentModule'
+    loadChildren: 'app/pages/content/content.module#ContentModule'
   }]
 }, {
   path: 'session',
   component: AuthLayoutComponent,
   children: [{
     path: '',
-    loadChildren: './pages/session/session.module#SessionModule'
+    loadChildren: 'app/pages/session/session.module#SessionModule'
   }]
 }, {
   path: '**',
