aa3d23fb683d726a61e7b2fc80557c04c22e6273
zhsyourai <zhsyourai@163.com>
2017-11-09 20:56:30 +0800

    Add online allow domain

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,7 +6,8 @@ import { JWT_OPTIONS, JwtModule } from '@auth0/angular-jwt';
 export function jwtOptionsFactory() {
   return {
     tokenGetter: () => localStorage.getItem('auth_token'),
-    whitelistedDomains: ['127.0.0.1:8080', '127.0.0.1:8081', '127.0.0.1:8085'],
+    whitelistedDomains: ['127.0.0.1:8080', '127.0.0.1:8081', '127.0.0.1:8085',
+      '69.30.199.90:8080', '69.30.199.90:8081', '69.30.199.90:8085'],
   };
 }
 
